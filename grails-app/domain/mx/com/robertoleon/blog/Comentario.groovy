d155818a013884d7fa745198cd8f233189f9b23b
Roberto <j.roberto.leon@gmail.com>
2013-02-12 17:10:46 -0600

    se agregan propiedades Usuario y fecha de creacion

--- grails-app/domain/mx/com/robertoleon/blog/Comentario.groovy
+++ grails-app/domain/mx/com/robertoleon/blog/Comentario.groovy
@@ -1,11 +1,17 @@
 package mx.com.robertoleon.blog
 
+import mx.com.robertoleon.seguridad.Usuario
+
 class Comentario {
 
     String titulo
+    Usuario usuario
     String descripcion
+    Date fechaCreacion
     static belongsTo  = [post:Post]
 
     static constraints = {
+        titulo(blank: false)
+        descripcion(blank: false)
     }
 }
